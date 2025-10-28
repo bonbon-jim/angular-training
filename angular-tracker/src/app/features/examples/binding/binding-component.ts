9810accadb5b34450879f3bb015833c6647f2442
Jinmin Fan <jim.jinmin.fan@outlook.com>
2025-10-28 15:02:42 +0800

    st

--- angular-tracker/src/app/features/examples/binding/binding-component.ts
+++ angular-tracker/src/app/features/examples/binding/binding-component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { IftaLabelModule } from 'primeng/iftalabel';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-binding-component',
-  imports: [],
+  imports: [FormsModule, InputTextModule, IftaLabelModule, ButtonModule],
   standalone: true,
   templateUrl: './binding-component.html',
   styleUrl: './binding-component.scss'
 })
 export class BindingComponent {
-
+  userName: string = '';
+  counter = signal(0);
 }
