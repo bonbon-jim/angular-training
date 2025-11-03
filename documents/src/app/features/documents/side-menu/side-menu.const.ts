59caf3d704a4663ddfaf004fa57fd7de6a73c5b3
Jinmin Fan <jim.jinmin.fan@outlook.com>
2025-11-03 09:21:50 +0800

    staging..

--- documents/src/app/features/documents/side-menu/side-menu.const.ts
+++ documents/src/app/features/documents/side-menu/side-menu.const.ts
@@ -3,15 +3,17 @@ import { MenuItem } from "./menu-item/menu-item.model";
 
 export const sideMenu: MenuItem[] = [
   {
+    icon: 'pi pi-desktop',
     label: 'Bindings',
     route: DocumentsRouting.Binding
   },
   {
+    icon: 'pi pi-code',
     label: '@Input',
     route: DocumentsRouting.InputPara
   },
   {
-    icon: 'pi',
+    icon: 'pi pi-cloud-download',
     label: 'API Call',
     route: DocumentsRouting.ApiCall
   },
